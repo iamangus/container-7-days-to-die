b62271238cdf18c5026b8b245f1837e12e99fa93
iamangus <turnbull.angus@gmail.com>
2023-07-11 12:01:08 -0500

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@ mkdir -p ~/7daystodie
 
 steamcmd +force_install_dir ~/7daystodie +login anonymous +app_update 294420 +quit
 
-bash ~/7daystodie/startserver.sh
+bash ~/7daystodie/startserver.sh -configfile=serverconfig.xml
