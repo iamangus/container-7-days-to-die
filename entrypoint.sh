ac3fd11bcee2c9d21f34f209d462ab09918d8fa2
iamangus <turnbull.angus@gmail.com>
2023-07-13 09:48:15 -0500

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ mkdir -p ~/7daystodie
 steamcmd +force_install_dir ~/7daystodie +login anonymous +app_update 294420 +quit
 
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
-bash ~/7daystodie/startserver.sh -configfile=serverconfig.xml
+bash ~/7daystodie/startserver.sh -configfile=serverconfig.xml -logfile "/dev/stdout"
